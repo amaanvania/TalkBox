75093f10492593068d6d9f1e46c775e6db2e91bf
amaanvania <amaanv@hotmail.com>
2019-01-28 01:19:05 -0500

    Updated GUI

--- TalkBox/src/config/Builder.java
+++ TalkBox/src/config/Builder.java
@@ -7,22 +7,16 @@ import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
-import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.Separator;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToolBar;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
 public class Builder extends Application{
@@ -36,6 +30,7 @@ public class Builder extends Application{
 	}
 	
 	public void newButtonPrompt(Stage primaryStage) {
+		
 		StackPane stackpane = new StackPane();
 		Label title = new Label("Welcome to TalkBox Configuration App");
 		title.setScaleX(2);
@@ -49,6 +44,7 @@ public class Builder extends Application{
 		hb.setSpacing(10);
 		stackpane.getChildren().addAll(title,hb);
 		StackPane.setAlignment(title, Pos.CENTER);
+		primaryStage.setTitle("TalkBox Config App");
         primaryStage.setScene(new Scene(stackpane,500,500));
         primaryStage.show();
         submit.setOnAction(new EventHandler<ActionEvent>() {
@@ -91,21 +87,51 @@ public class Builder extends Application{
 			if(i > 0 && i % 6 == 0){
 				increment++;
 			}
-			AudioButton y = new AudioButton();
+			//AudioButton y = new AudioButton();
 			ImageView iv1 = new ImageView();
 			iv1.setImage(img);
 			iv1.setFitWidth(100);
 			iv1.setPreserveRatio(true);
 			iv1.setSmooth(true);
 			iv1.setCache(true);
-			Button x = new Button("Choose image:");
-			x.setPrefSize(100, 20);
-			x.setOnAction(new EventHandler<ActionEvent>(){
+			Button edit = new Button("Edit Button:");
+			edit.setPrefSize(100, 20);
+			edit.setOnAction(new EventHandler<ActionEvent>(){
 				@Override
 				public void handle(ActionEvent event) {
-					y.chooseImagePath();
+					Stage x = new Stage();
+					GridPane g = Utilities.setEditPrompt(x);
+					Button submit = new Button("Submit");
+					TextField textField = new TextField();
+					GridPane.setConstraints(submit, 4, 4);
+					GridPane.setConstraints(textField, 1, 0);
+					g.getChildren().add(submit);
+					g.getChildren().add(textField);
+					x.setScene(new Scene(g));
+					x.show();
+					submit.setOnAction(new EventHandler<ActionEvent>() {
+						@Override
+						public void handle(ActionEvent e) {;
+								try {
+									edit.setText(textField.getText());
+									Image n = new Image(new FileInputStream(Utilities.ImagePath));
+									iv1.setImage(n);
+									iv1.setFitWidth(100);
+									iv1.setPreserveRatio(true);
+									iv1.setSmooth(true);
+									iv1.setCache(true);
+								} catch (FileNotFoundException e1) {
+									// TODO Auto-generated catch block
+									e1.printStackTrace();
+								}
+								x.close();
+							}
+						
+					});
+					
+					/**
 					try {
-						Image n = new Image(new FileInputStream(y.getImagePath()));
+						Image n = new Image(new FileInputStream(Utilities.ImagePath));
 						iv1.setImage(n);
 						iv1.setFitWidth(100);
 						iv1.setPreserveRatio(true);
@@ -115,16 +141,17 @@ public class Builder extends Application{
 						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
+					*/
 					
 				}
 			});
-			gridpane.setConstraints(x, i % 6, 2 + 2 * increment);
-			gridpane.setConstraints(iv1, i % 6, 1 + 2 * increment);
-			gridpane.getChildren().addAll(iv1,x);
+			GridPane.setConstraints(edit, i % 6, 2 + 2 * increment);
+			GridPane.setConstraints(iv1, i % 6, 1 + 2 * increment);
+			gridpane.getChildren().addAll(iv1,edit);
 		}
 		ToolBar bar = buildToolbar();
 		gridpane.getChildren().add(bar);
-		gridpane.setConstraints(bar, 0, 0);
+		GridPane.setConstraints(bar, 0, 0);
 		primaryStage.setScene(new Scene(gridpane));
 		primaryStage.show();
 	}
--- TalkBox/src/config/Utilities.java
+++ TalkBox/src/config/Utilities.java
@@ -2,17 +2,81 @@ package config;
 
 import java.io.File;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 public class Utilities {
-	
+
+	public static String text;
+	public static String AudioPath;
+	public static String ImagePath;
+
 	public static String fileChoose(Stage mainStage) {
-		 FileChooser fileChooser = new FileChooser();
-		 File selectedFile = fileChooser.showOpenDialog(mainStage);
-		 if (selectedFile != null) {
-		    return selectedFile.getAbsolutePath();
-		 }
-		 return null;
+		FileChooser fileChooser = new FileChooser();
+		File selectedFile = fileChooser.showOpenDialog(mainStage);
+		if (selectedFile != null) {
+			return selectedFile.getAbsolutePath();
+		}
+		return null;
+	}
+
+	public static GridPane setEditPrompt(Stage primaryStage) {
+		GridPane gridpane = new GridPane();
+		gridpane.setVgap(10);
+		gridpane.setHgap(10);
+		primaryStage.setTitle("Configure Button");
+		//Button submit = new Button("Submit");
+		Button audio = new Button("Choose AudioFile");
+		Button image = new Button("Choose Image");
+		Label label1 = new Label("Set Title:");
+		
+		TextField audioField = new TextField();
+		TextField imageField = new TextField();
+		GridPane.setConstraints(label1, 0, 0);
+		GridPane.setConstraints(audio, 0, 1);
+		GridPane.setConstraints(audioField, 1, 1);
+		GridPane.setConstraints(image, 0, 2);
+		GridPane.setConstraints(imageField, 1, 2);
+		//gridpane.setConstraints(submit, 4, 4);
+		gridpane.getChildren().addAll(label1, audio, image,imageField, audioField);
+		/*submit.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent e) {
+				if ((textField.getText() != null && !textField.getText().isEmpty())) {
+					text = textField.getText();
+					primaryStage.close();
+				}
+			}
+		});*/
+		image.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent e) {
+				AudioButton b = new AudioButton();
+				b.chooseImagePath();
+				imageField.setText(b.getImagePath());
+				ImagePath = b.getImagePath();
+			}
+		});
+		audio.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent e) {
+				AudioButton b = new AudioButton();
+				b.chooseAudioPath();
+				audioField.setText(b.getAudioPath());
+				AudioPath = b.getAudioPath();
+			}
+		});
+		return gridpane;
+
+	}
+
+	public static String getText() {
+		return text;
 	}
 }
