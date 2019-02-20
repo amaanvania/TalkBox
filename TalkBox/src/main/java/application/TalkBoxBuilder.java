4d2bff7ff4a0a0ced2487cae9e5b93922b037b31
amaanvania <amaanv@hotmail.com>
2019-02-20 01:39:02 -0500

    fixed small problem

--- TalkBox/src/main/java/application/TalkBoxBuilder.java
+++ TalkBox/src/main/java/application/TalkBoxBuilder.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import main.java.config.Builder;
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.layout.GridPane;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class TalkBoxBuilder extends Application{ //simple class that "runs" talk box app
@@ -20,7 +20,7 @@ public class TalkBoxBuilder extends Application{ //simple class that "runs" talk
 	public void start(Stage primaryStage) throws Exception {
 		// TODO Auto-generated method stub
 		TalkBoxApp a = new TalkBoxApp(new Builder());
-		GridPane b = a.getGridpane();
+		BorderPane b = a.getPane();
 		primaryStage.setTitle("TalkBox Application");
 		primaryStage.setScene(new Scene(b));
 		primaryStage.show();
