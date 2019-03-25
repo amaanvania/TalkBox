649e803308d0644588e6c8b45d56ea910ba27c21
Nasiff <nasif_fcb_haque@yahoo.com>
2019-03-24 22:15:45 -0400

    Added Generic Statistic Class

---
+++ TalkBox/src/log/Stat.java
@@ -0,0 +1,32 @@
+package log;
+
+public class Stat {
+	// Statistics for the TalkBox Configuration Application
+	public static int TestCounter = 0;
+	public static int SaveCounter = 0;
+	public static int RecordAudioCounter = 0;
+	public static int AddNewCounter = 0;
+	public static int DeleteLastCounter = 0;
+	public static int DeleteSpecificCounter = 0;
+	
+	public static int AudioButtonsCreated = 0;
+	public static int AutofillAudioButtonCounter = 0;
+	public static int DragDropAudioButtonCounter = 0;
+	public static int TitlesSet = 0;
+	public static int ImagesSelected = 0;
+	public static int AudioTracksSelected = 0;
+	
+	// Statistics for the TalkBox Simulator Application
+	public static int AudioButtonsPlayed = 0;
+	public static int AudioButtonsStopped= 0; // Also applies to the Configuration Application
+	public static int AudioButtonsEdited = 0;
+	
+	
+	// General Statistics
+	public static int VolumeCounter = 0;
+	public static int UserManualCounter = 0;
+	public static int ReportIssueCounter = 0;
+	public static int ContactUsCounter = 0;
+	
+
+}
